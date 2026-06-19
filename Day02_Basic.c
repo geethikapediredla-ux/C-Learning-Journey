7c5a8b50c82d7e03cc9c6cf4ac1dd70200828e89
Geetika <geethikapediredla@gmail.com>
2026-06-19 20:44:18 +0530

    Add files via upload

---
+++ Day02_Basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+// This prints Hey! I an Geetika.
+/*I am a first year ECE student
+I like Novels<3
+*/
+printf("Hey! I am Geetika.\n");
+printf("I am a first year ECE student.\n");
+printf("I like Novels.");
+return 0;
+}
